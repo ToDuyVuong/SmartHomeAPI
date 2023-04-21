126f26656f683d1f15eac785f66e86b87ae81cd5
ToDuyVuong <comc5288@gmail.com>
2023-04-21 22:04:26 +0700

    Initial commit

---
+++ src/test/java/vn/smarthome/smarthomeapi/SmartHomeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.smarthome.smarthomeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHomeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
